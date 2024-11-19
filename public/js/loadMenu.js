942b04e30a878f671d552fe01eba3c093e9e3306
Krystian Paryła <krystian.paryla@o2.pl>
2024-11-19 18:09:58 +0100

    sass and bootstrap introduced, added theme switching module

--- public/js/loadMenu.js
+++ public/js/loadMenu.js
@@ -7,5 +7,17 @@ function loadMenu() {
 }
 
 
+function toggleTheme() {
+  const body = document.body;
+  if (body.classList.contains('light-theme')) {
+    body.classList.remove('light-theme');
+    body.classList.add('dark-theme');
+  } else {
+    body.classList.remove('dark-theme');
+    body.classList.add('light-theme');
+  }
+}
 
+// Domyślny motyw
+document.body.classList.add('light-theme');
 
